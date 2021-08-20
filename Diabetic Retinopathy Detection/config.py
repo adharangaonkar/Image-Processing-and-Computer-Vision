fdf468bba940ee69efd2a58e47d92e2bedc32d07
adharangaonkar <adharangaonkar@gmail.com>
2021-08-20 00:48:09 -0400

    Diabetic Retinopathy Detection baseline-1

--- Diabetic Retinopathy Detection/config.py
+++ Diabetic Retinopathy Detection/config.py
@@ -3,4 +3,45 @@
 ## TODO
 
 
+
 import torch
+import albumentations as A # used for data augmentation
+from albumentations.pytorch import ToTensorV2
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+LEARNING_RATE = 3e-5
+WEIGHT_DECAY = 5e-4
+BATCH_SIZE = 20
+NUM_EPOCHS = 100
+NUM_WORKERS = 6
+CHECKPOINT_FILE = "b3.pth.tar" # the file where we are going to save the model at
+PIN_MEMORY = True
+SAVE_MODEL = True
+LOAD_MODEL = True
+
+# Image Augmentations
+
+train_transforms = A.compose(
+    [
+        A.Resize(width= 150, height =150),
+        A.RandomCrop(height= 120, width=120),
+        A.Normalize(
+            mean=[0.3199, 0.2240, 0.1609],
+            std=[0.3020, 0.2183, 0.1741],
+            max_pixel_value=255.0,
+        ),
+        ToTensorV2(),
+    ]
+)
+
+val_transforms = A.Compose(
+    [
+        A.Resize(height=728, width=728),
+        A.Normalize(
+            mean=[0.3199, 0.2240, 0.1609],
+            std=[0.3020, 0.2183, 0.1741],
+            max_pixel_value=255.0,
+        ),
+        ToTensorV2(),
+    ]
+)
--- Diabetic Retinopathy Detection/dataset.py
+++ Diabetic Retinopathy Detection/dataset.py
@@ -1,1 +1,42 @@
 # Dataset file to load the data
+
+import config
+import os
+import pandas as pd
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+from tqdm import tqdm
+
+
+class DRDataset(Dataset):
+    def __init__(self, images_folder, path_to_csv, train=True, transform=None):
+        super().__init__()
+        self.data = pd.read_csv(path_to_csv)
+        self.images_folder = images_folder
+        self.image_files = os.listdir(images_folder)
+        self.transform = transform
+        self.train = train
+
+    def __len__(self):
+        return self.data.shape[0] if self.train else len(self.image_files)
+        # if the length of the train data is to be calculated simply the length of the csv will do
+        # else for the validation set, as there is no csv, we will calculate the number of images
+
+    def __getitem__(self, index):
+        if self.train:
+            image_file, label = self.data.iloc[index]
+        else:
+            # if test simply return -1 for label, I do this in order to
+            # re-use same dataset class for test set submission later on
+            image_file, label = self.image_files[index], -1
+            image_file = image_file.replace(".jpeg", "")
+
+        image = np.array(Image.open(os.path.join(self.images_folder, image_file + ".jpeg")))
+        # opening with PIL and converting it into a numpy image
+
+        # Image Augmentation
+        if self.transform:
+            image = self.transform(image=image)["image"]
+
+        return image, label, image_file
